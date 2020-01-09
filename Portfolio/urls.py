a5232401308312b5d95208bffe8783fef29ebbbf
Mahdibayat <mahdibayat72@hotmail.com>
2020-01-09 13:51:58 +0330

    second design

--- Portfolio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'Portfolio'
-
-urlpatterns = [
-    url(r'^/', views.index)
-]
--- Portfolio/views.py
+++ Portfolio/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    render(request, 'Portfolio/html.html')
+    return render(request, 'Portfolio/html.html')
+
